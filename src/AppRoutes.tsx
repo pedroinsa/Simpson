fb2ce518f5ee717369a9d549e3a323f41b1020f4
Pedro Insausti <pedro@pedro-i5>
2023-07-25 23:49:35 -0300

    second commit

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import { Landing } from './components/Landing'
 import './App.css'
 
 export const AppRoutes = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className="AppRoutes">
                 <Routes>
                     <Route path="/" element={<Landing />} />
@@ -14,6 +14,6 @@ export const AppRoutes = () => {
 
                 </Routes>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
